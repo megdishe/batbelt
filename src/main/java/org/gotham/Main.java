08427bec6e1111a5f98b22bfcaf1def15b57fd8e
megdishe-myga <m.megdishe@gmail.com>
2024-01-02 16:40:47 +0100

    feat(BatBelt): add unit test

--- src/main/java/org/gotham/Main.java
+++ src/main/java/org/gotham/Main.java
@@ -4,15 +4,19 @@ import java.util.Set;
 
 import static org.gotham.BatGadget.BATARANG;
 import static org.gotham.BatGadget.LINE_LAUNCHER;
+import static org.gotham.BatGadget.REMOTE_HACKING_DEVICE;
 import static org.gotham.BatGadget.SMOKE_PELLET;
 
 public class Main {
 
     public static void main(String[] args) {
         BatmanBelt batmanBelt = BatmanBelt.builder()
-                .batGadgets(Set.of(BATARANG, LINE_LAUNCHER, SMOKE_PELLET))
+                .batGadgets(Set.of(BatGadget.values()))
                 .build();
         batmanBelt.useAppropriateGadget(Situation.COVER);
+        batmanBelt.useAppropriateGadget(Situation.ACCESS_HIGH_AREAS);
+        batmanBelt.useAppropriateGadget(Situation.STUN);
+        batmanBelt.useAppropriateGadget(Situation.HACK);
     }
 
 }
